7c18f345754c7d8353978b3b0da36868ebc610bd
Daniel Esteban <conejo@conejo.me>
2016-01-19 13:40:28 +0100

    fix

--- masjob/search_list.php
+++ masjob/search_list.php
@@ -22,7 +22,7 @@
  <table border="0" cellspacing="0">
      <tbody>
         <?php while(osc_has_items()) { ?>
-            <tr class="row r<?php echo osc_item_id() ; ?>" rel="r<?php echo osc_item_id() ; ?>">
+            <tr class="row r<?php echo osc_item_id() ; ?> <?php osc_run_hook("highlight_class"); ?>" rel="r<?php echo osc_item_id() ; ?>">
                  <td class="title">
                      <h3><a href="<?php echo osc_item_url() ; ?>"><strong><?php echo osc_item_title() ; ?></strong> <?php echo osc_item_city() ; ?></a></h3>
                  </td>
